e1272d58103eb12ca09da0b2e60d028764be7181
gsus2024 <gsusromerovillanueva2024@gmail.com>
2025-06-18 08:39:24 -0500

    se agrega conexion.php desde ramagsus

--- cargarfamilias.php
+++ cargarfamilias.php
@@ -1,1 +1,5 @@
 <h1>Listado de familias</h1>
+<?php
+    require_once 'conexion.php';
+    $sql="select * from familia";
+?>
---
+++ conexion.php
@@ -0,0 +1,6 @@
+<?php
+    $url='mysql: host=localhost; dbname=ventasdb';
+    $user='root';
+    $password='';
+    $cn=new PDO($url,$user,$password);
+?>
